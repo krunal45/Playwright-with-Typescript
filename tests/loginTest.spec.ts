9720ce270cbacd2d08d01b0d945fe7738a559f94
qaprayogshala <qaprayogshala@gmail.com>
2025-03-18 16:12:09 +0530

    login Test & config file changes

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,7 +1,22 @@
 import{test,expect} from '@playwright/test';
 
-test('login test', async ({page}) => {
+test('Verify Page Title', async ({page}) => {
 await page.goto('https://www.saucedemo.com/');
 console.log(await page.title());
 await expect(page).toHaveTitle('Swag Labs');
 })
+
+test('Verify Login ', async ({page}) => {
+await page.goto('https://www.saucedemo.com/');
+await page.fill('#user-name','standard_user');
+await page.fill('#password','secret_sauce');
+await page.click('#login-button');
+expect(await page.locator('a.shopping_cart_link')).toBeVisible();
+}
+);
+
+// test.afterAll(async ({page}) => {
+// await page.click('button#react-burger-menu-btn');
+// await page.click('a#logout_sidebar_link');
+// expect(await page.locator('input#user-name')).toBeVisible();
+// })
